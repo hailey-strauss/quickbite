ea0353881c9d5932a446fab907abe1a0e47d550b
Hailey Strauss <hstrauss2001@gmail.com>
2025-05-14 18:57:26 -0400

    server error

--- src/server.js
+++ src/server.js
@@ -4,25 +4,29 @@ import app from "./app.js";
 
 dotenv.config();
 
-let isConnected = false;
+//let isConnected = false;
 
 export default async function handler(req, res) {
   try {
-    console.log("Incoming request:", req.url);
+    console.log("Incoming request:", req);
 
-    if (!isConnected) {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(" MongoDB connected");
-      isConnected = true;
-    }
+    // if (!isConnected) {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(" MongoDB connected");
+    //isConnected = true;
+    //}
 
-    return app(req, res); // Forward to Express app
+    // return app(req, res); // Forward to Express app
   } catch (err) {
     console.error(" Server error:", err);
-    res.statusCode = 500;
-    res.end("Internal Server Error");
+    //res.statusCode = 500;
+    //res.end("Internal Server Error");
   }
 }
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Server running on port ${process.env.PORT}`);
+  handler(req, res);
+});
