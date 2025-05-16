606b4f9e4fc9619bff5a196283aea1fd491ce591
mahmadsadikRepositry <sadikkoliya@gmail.com>
2025-05-15 23:25:20 -0400

    Commented DB call

--- src/server.js
+++ src/server.js
@@ -6,25 +6,25 @@ dotenv.config();
 
 //let isConnected = false;
 
-export default async function connectDB() {
-  try {
+// export async function connectDB() {
+//   try {
 
-    // if (!isConnected) {
-    await mongoose.connect(process.env.MONGO_URI, {
-    });
-    console.log(" MongoDB connected");
-    //isConnected = true;
-    //}
+//     // if (!isConnected) {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//     });
+//     console.log(" MongoDB connected");
+//     //isConnected = true;
+//     //}
 
-    // return app(req, res); // Forward to Express app
-  } catch (err) {
-    console.error(" Server error:", err);
-    //res.statusCode = 500;
-    //res.end("Internal Server Error");
-  }
-}
+//     // return app(req, res); // Forward to Express app
+//   } catch (err) {
+//     console.error(" Server error:", err);
+//     //res.statusCode = 500;
+//     //res.end("Internal Server Error");
+//   }
+// }
 
-await connectDB();
+// await connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
