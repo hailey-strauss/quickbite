bb1801acaa50cc3371d8fd571b6fde1f79ee672d
Hailey Strauss <hstrauss2001@gmail.com>
2025-05-19 11:13:06 -0400

    restore commented-out route handlers

--- src/app.js
+++ src/app.js
@@ -6,25 +6,24 @@ import userRouter from "./routes/userRoutes.js";
 import restaurantRoutes from "./routes/restaurantRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.get('/health', (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({
-    status: 'OK',
-    message: 'Server is running',
+    status: "OK",
+    message: "Server is running",
     timestamp: new Date().toISOString(),
     uptime: process.uptime(),
-    environment:  'Production'
+    environment: "Production",
   });
 });
 
-// app.use("/api/users", userRouter);
-// app.use("/api/restaurants", restaurantRoutes);
-// app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRouter);
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/orders", orderRoutes);
 
 export default app;
